42908c9375d2975b1437687122db0164da659d9a
tcourdy <freedomfighter1986@gmail.com>
2015-10-25 16:52:01 -0600

    Added pre processing code and added the -g flag to the makefile for debugging

--- satsolv.h
+++ satsolv.h
@@ -9,6 +9,7 @@
 #define DEBUG 1     // enables debugging output
 #define MAXLINE 256 // maximum length of a line
 #define MAXCLAUSES 256 // maximum number of clauses in a formula
+#define ERROR_STRING "ERROR\n" /*define the error string*/
 
 /* Core algorithm */
 int solve(FILE *fp);
@@ -16,5 +17,8 @@ int solve(FILE *fp);
 /* Helper functions */
 void get_clauses(char *clauses[], FILE *fp);
 void print_clauses(char *clauses[], int nclauses);
+
+void pre_process(FILE *fp);
+long int convert_to_int(char *pch);
 
 #endif
