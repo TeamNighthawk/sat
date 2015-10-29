3ac15e4f3593fb1b6a9d9e13654817cfc4c0f0ca
Jay Tuckett <12crown4@gmail.com>
2015-10-29 14:37:18 -0600

    Modified Make file and test files to use new repo design

--- test/tester.py
+++ test/tester.py
@@ -7,20 +7,20 @@ from FileCreator import SatFileInstance
 failed_cnt = 0
 
 def run_test(name):
-  """
-  Runs the sat solver on the name supplied. Also runs the sat solver on the oracle
-  and compares the results of the two.
-  """
-  print ("Testing with file: %s " % name)
-  output = subprocess.check_output("./satsolv %s" % name, shell=True)
-  expected = ""
-  if output != expected:
-    global failed_cnt
+    """
+    Runs the sat solver on the name supplied. Also runs the sat solver on the oracle
+    and compares the results of the two.
+    """
+    print ("Testing with file: %s " % name)
+    output = subprocess.check_output("../bin/satsolv %s" % name, shell=True)
+    expected = ""
+    if output != expected:
+        global failed_cnt
     failed_cnt += 1
     print ("FAILED TEST: '%s'\n\tEXPECTED: %s\r\tRETURNED: %s\n") % (name, expected, output)
 
 if __name__ == "__main__":
-  subprocess.call(['make'], shell=True)
+    subprocess.call(['make -C ../'], shell=True)
 
 SOLVER = SatFileInstance(5, 3)
 
